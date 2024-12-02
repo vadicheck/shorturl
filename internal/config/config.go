6438667f8e7870920bbf2f9505481b05bc2bfb3e
chvss <wadim199602@gmail.com>
2024-12-02 13:28:05 +0700

    iter5

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,21 +12,21 @@ var Config struct {
 }
 
 func ParseFlags() {
-	Config.ServerAddress = os.Getenv("SERVER_ADDRESS")
-	Config.BaseURL = os.Getenv("BASE_URL")
-	Config.StoragePath = os.Getenv("STORAGE_PATH")
-
-	if Config.ServerAddress == "" {
-		flag.StringVar(&Config.ServerAddress, "a", "localhost:8080", "HTTP server startup address")
-	}
-
-	if Config.BaseURL == "" {
-		flag.StringVar(&Config.BaseURL, "b", "http://localhost:8080", "the base address of the resulting shortened URL")
-	}
-
-	if Config.StoragePath == "" {
-		flag.StringVar(&Config.StoragePath, "storage-path", "", "path to storage")
-	}
+	flag.StringVar(&Config.ServerAddress, "a", "localhost:8080", "HTTP server startup address")
+	flag.StringVar(&Config.BaseURL, "b", "http://localhost:8080", "the base address of the resulting shortened URL")
+	flag.StringVar(&Config.StoragePath, "storage-path", "", "path to storage")
 
 	flag.Parse()
+
+	if serverAddress := os.Getenv("SERVER_ADDRESS"); serverAddress != "" {
+		Config.ServerAddress = serverAddress
+	}
+
+	if baseURL := os.Getenv("BASE_URL"); baseURL != "" {
+		Config.BaseURL = baseURL
+	}
+
+	if storagePath := os.Getenv("STORAGE_PATH"); storagePath != "" {
+		Config.StoragePath = storagePath
+	}
 }
